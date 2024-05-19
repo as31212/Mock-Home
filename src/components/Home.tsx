c282c7983046ea6daba48b68fe5ae4a5488d9e5c
Ahmad Searcy <ahmadsearcy7@gmail.com>
2024-05-19 12:16:25 -0500

    home page additions

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,12 +4,18 @@ import { MdManageSearch } from "react-icons/md";
 import { FiEye } from "react-icons/fi";
 import { FaHouseFlag } from "react-icons/fa6";
 import { FaAward } from "react-icons/fa6";
-
+import { RiMoneyDollarCircleLine } from "react-icons/ri";
+import { MdAddHome } from "react-icons/md";
+import { FaFireAlt } from "react-icons/fa";
+import { LuSmilePlus } from "react-icons/lu";
 
 const Home: React.FC = () => {
   return (
     <>
-      <div id="home-page-one" className="h-auto flex flex-col lg:flex-row justify-center items-center bg-orange-50">
+      <div
+        id="home-page-one"
+        className="h-auto min-h-screen flex flex-col lg:flex-row justify-center items-center bg-orange-50"
+      >
         <div className="w-full lg:w-1/2">
           <img
             className="w-full h-auto object-cover"
@@ -17,14 +23,20 @@ const Home: React.FC = () => {
             alt="Home page img"
           />
         </div>
-        <div className="h-fit w-full lg:w-1/3 p-10 flex flex-col gap-10" id="home-text-box">
-          <h2 className="text-4xl lg:text-5xl font-semibold text-center lg:text-left">
-            Find the property you love
+        <div
+          className="h-fit w-full lg:w-1/3 p-10 flex flex-col gap-10"
+          id="home-text-box"
+        >
+          <h2 className="text-4xl lg:text-5xl font-bold text-center lg:text-left">
+            Find the perfect property you love
           </h2>
           <p className="text-black text-center lg:text-left">
             We help people find homes they want at an affordable price
           </p>
-          <div className="bg-white p-8 rounded-lg shadow-lg flex flex-col" id="search">
+          <div
+            className="bg-white p-8 rounded-lg shadow-lg flex flex-col"
+            id="search"
+          >
             <div className="flex flex-wrap gap-4 justify-center" id="buttons">
               <button className="w-5/12 shadow font-semibold px-5 py-3 rounded-lg bg-gray-300 hover:bg-black hover:text-white duration-300">
                 Buy
@@ -64,24 +76,139 @@ const Home: React.FC = () => {
           </div>
         </div>
       </div>
-      <div id="home-page-two" className=" min-h-screen flex justify-center items-center p-5 gap-5">
-      <div className="page-2-large-div h-[500px] w-1/2 bg-orange-200 rounded-2xl flex flex-col justify-center gap-5 p-5 ">
-        <h2 className="font-bold text-2xl">Home acquisition and rental made simple</h2>
-        <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque pariatur, cupiditate beatae enim inventore natus aut,.</p>
-        <Link to='/Listing'>
-          <button className="bg-black text-white w-32 py-3 rounded-lg ">
-           Get Started
-          </button>
-        </Link>
+      <div
+        id="home-page-two"
+        className="py-20 flex justify-center items-center p-5 gap-5"
+      >
+        <div className="page-2-large-div h-[500px] w-1/2 bg-orange-200 rounded-2xl flex flex-col justify-center gap-5 p-5 ">
+          <h2 className="font-bold text-2xl">
+            Home acquisition and rental made simple
+          </h2>
+          <p className="">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+            pariatur, cupiditate beatae enim inventore natus aut,.
+          </p>
+          <Link to="/Listing">
+            <button className="bg-black text-white w-32 py-3 rounded-lg ">
+              Get Started
+            </button>
+          </Link>
+        </div>
+        <div className="page-2-large-div min-w-96 h-[500px] w-1/3 flex flex-wrap gap-2">
+          <div className="bg-orange-50 w-5/12 h-5/12 rounded-lg flex flex-col justify-center font-bold text-center text-3xl p-5 gap-3 home-2-box">
+            <MdManageSearch className="mx-auto text-3xl text-orange-400" />
+            <h2>Search your location</h2>
+          </div>
+          <div className="bg-orange-50 w-5/12 h-5/12 rounded-lg flex flex-col justify-center font-bold text-center text-3xl p-5 gap-3 home-2-box">
+            <FaHouseFlag className="mx-auto text-3xl text-orange-400" />
+            <h2>Get your dream house</h2>
+          </div>
+          <div className="bg-orange-50 w-5/12 h-5/12 rounded-lg flex flex-col justify-center font-bold text-center text-3xl p-5 gap-3 home-2-box">
+            <FiEye className="mx-auto text-3xl text-orange-400" />
+            <h2>Detailed Walk-Throughs</h2>
+          </div>
+          <div className="bg-orange-50 w-5/12 h-5/12 rounded-lg flex flex-col justify-center font-bold text-center text-3xl p-5 gap-3 home-2-box">
+            <FaAward className="mx-auto text-3xl text-orange-400" />
+            <h2>World class agents</h2>
+          </div>
+        </div>
       </div>
-      <div className="page-2-large-div min-w-96 h-[500px] w-1/3 flex flex-wrap gap-2">
-    <div className="bg-orange-50 w-5/12 h-5/12 rounded-lg flex flex-col justify-center font-bold text-center text-3xl p-5 gap-3 home-2-box"><MdManageSearch className="mx-auto text-3xl text-orange-400"/><h2>Search your location</h2></div>
-    <div className="bg-orange-50 w-5/12 h-5/12 rounded-lg flex flex-col justify-center font-bold text-center text-3xl p-5 gap-3 home-2-box"><FaHouseFlag className="mx-auto text-3xl text-orange-400"/>
-<h2>Get your dream house</h2></div>
-    <div className="bg-orange-50 w-5/12 h-5/12 rounded-lg flex flex-col justify-center font-bold text-center text-3xl p-5 gap-3 home-2-box"><FiEye className="mx-auto text-3xl text-orange-400"/><h2>Detailed Walk-Throughs</h2></div>
-    <div className="bg-orange-50 w-5/12 h-5/12 rounded-lg flex flex-col justify-center font-bold text-center text-3xl p-5 gap-3 home-2-box"><FaAward className="mx-auto text-3xl text-orange-400"/><h2>World class agents</h2></div>
+      <div
+        id="home-page-three"
+        className="h-auto flex gap-5 justify-center bg-[#FFFAF7]"
+      >
+        <div className="w-[400px] h-fit p-10 flex flex-col gap-5 justify-center items-center">
+          <div className="circle-icon-wrap bg-white rounded-full shadow-2xl p-5 w-[60px] h-[60px] flex justify-center items-center">
+            <RiMoneyDollarCircleLine className=" text-orange-400 text-7xl scale-[200%]" />
+          </div>
+          <h2 className="font-bold text-4xl">$204.2M</h2>
+          <div className="text-wrapper text-center">
+            <p className="text-[#4C788D] font-semibold text-2xl">
+              From in-site
+            </p>
+            <p className="text-[#4C788D] font-semibold text-2xl">
+              property transactions
+            </p>
+          </div>
+        </div>
+        <div className="w-[400px] h-fit p-10 flex flex-col gap-5 justify-center items-center">
+          <div className="circle-icon-wrap bg-white rounded-full shadow-2xl p-5 w-[60px] h-[60px] flex justify-center items-center">
+            <MdAddHome className=" text-orange-400 text-7xl scale-[200%]" />
+          </div>
+          <h2 className="font-bold text-4xl">30K+</h2>
+          <div className="text-wrapper text-center">
+            <p className="text-[#4C788D] font-semibold text-2xl">Properties</p>
+            <p className="text-[#4C788D] font-semibold text-2xl">
+              to choose from
+            </p>
+          </div>
+        </div>
+        <div className="w-[400px] h-fit p-10 flex flex-col gap-5 justify-center items-center">
+          <div className="circle-icon-wrap bg-white rounded-full shadow-2xl p-5 w-[60px] h-[60px] flex justify-center items-center">
+            <FaFireAlt className=" text-orange-400 text-7xl scale-[150%]" />
+          </div>
+          <h2 className="font-bold text-4xl">700</h2>
+          <div className="text-wrapper text-center">
+            <p className="text-[#4C788D] font-semibold text-2xl">Daily</p>
+            <p className="text-[#4C788D] font-semibold text-2xl">
+              property transactions
+            </p>
+          </div>
+        </div>
+        <div className="w-[400px] h-fit p-10 flex flex-col gap-5 justify-center items-center">
+          <div className="circle-icon-wrap bg-white rounded-full shadow-2xl p-5 w-[60px] h-[60px] flex justify-center items-center">
+            <LuSmilePlus className=" text-orange-400 text-7xl scale-[200%]" />
+          </div>
+          <h2 className="font-bold text-4xl">10,000+</h2>
+          <div className="text-wrapper text-center">
+            <p className="text-[#4C788D] font-semibold text-2xl">
+              Regular Clients
+            </p>
+          </div>
+        </div>
       </div>
+      <div id="home-page-four" className="flex flex-wrap p-5 justify-center items-center min-h-screen h-auto bg-orange-50">
+        <div className="text-page-4 w-[45vw] flex flex-col gap-5 justify-center ">
+          <h2 className="text-4xl font-bold">Find Agents That Work For You</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore est
+            inventore tempore quos cum ipsa modi nam ad numquam cumque adipisci,
+            quis deleniti autem incidunt facere aut! Soluta, unde quae?
+          </p>
+          <Link to="/Agents">
+            <button className="bg-black text-white w-32 py-3 rounded-lg ">
+              Get Started
+            </button>
+          </Link>
+        </div>
+        <div id="img-div-home-page-4" className="w-auto min-w-96 flex flex-col flex-wrap gap-3 h-[500px]">
+          <img className="w-80 special-image-home-page-4 h-[180px] rounded-lg" src="https://www.legalzoom.com/sites/lz.com/files/inline-images/man-woman-look-over-documents-with-agent.jpg" alt="agent talking to clients" />
+          <img className="w-80 images-home-page-4 h-[300px] rounded-lg" src="https://www.thestaffingstream.com/wp-content/uploads/2015/07/ThinkstockPhotos-465328301.jpg" alt="agent handing over keys" />
+          <img className="w-80 images-home-page-4 h-[300px] rounded-lg" src="https://lirp.cdn-website.com/28156074/dms3rep/multi/opt/01+-+True+Homes+-+Huntley+-+R02-3x2-1920w.jpg" alt="home" />
+          <img className="w-80 images-home-page-4 h-[180px] rounded-lg" src="https://www.thespruce.com/thmb/S1aye-s9z6VRA58-V8oRLSQwKCc=/5100x0/filters:no_upscale():max_bytes(150000):strip_icc()/SPR-luxury-kitchens-5211364-hero-688d716970544978bc12abdf17ce6f83.jpg" alt="kitchen" />
+        </div>
       </div>
+
+      <div id="home-page-five" className="flex gap-5 flex-row-reverse flex-wrap p-5 justify-center items-center min-h-screen h-auto">
+      <div className="text-page-5 w-[45vw] flex flex-col gap-5 justify-center ">
+          <h2 className="text-4xl font-bold">Top rated online realty service</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore est
+            inventore tempore quos cum ipsa modi nam ad numquam cumque adipisci,
+            quis deleniti autem incidunt facere aut! Soluta, unde quae?
+          </p>
+          <Link to="/Agents">
+            <button className="bg-black text-white w-32 py-3 rounded-lg ">
+              Get Started
+            </button>
+          </Link>
+        </div>
+        <div className="">
+          <img className=" rounded-lg w-auto  max-w-[800px]" src="https://images.squarespace-cdn.com/content/v1/58420910f5e23112c08b8068/1486010598453-1F2QJGVXDTZLGMI8IM1V/1423PalomaAve_5492.jpg?format=2500w" alt="home-page-4" />
+          <div id="background-img-page-4" className=""></div>
+        </div>
+      </div>
+
       <Footer />
     </>
   );
